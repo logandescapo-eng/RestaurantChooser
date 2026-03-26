21fa2ba0556d0b5243e625383119a72558bfdcaa
Manye Patrice <logandescapo@gmail.com>
2026-03-26 12:35:17 +0500

    week 3-5: RestaurantChooser app part 1

---
+++ RestaurantChooser/components/navigation.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import PeopleScreen from "../screens/people/peopleScreen";
+import DecisionScreen from "../screens/decision/decisionScreen";
+import RestaurantsScreen from "../screens/restaurants/restaurantsScreen";
+import { Image, Platform } from "react-native";
+import Constants from "expo-constants";
+import { StatusBar } from "expo-status-bar";
+
+const platformOS = Platform.OS.toLowerCase();
+const Tab = createMaterialTopTabNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Tab.Navigator
+        initialRouteName="Restaurants"
+        tabBarPosition={platformOS === "ios" ? "bottom" : "top"}
+        screenOptions={{
+          animationEnabled: true,
+          swipeEnabled: true,
+          lazy: true,
+          tabBarShowIcons: true,
+          tabBarIndicatorStyle: { backgroundColor: "#555555" },
+          tabBarActiveTintColor: "#ff0000",
+          tabBarStyle: {
+            paddingTop: platformOS === "android" ? Constants.statusBarHeight : 0,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="People"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Image
+                source={require("../assets/people.jpg")}
+                style={{ width: 32, height: 32, tintColor: color }}
+              />
+            ),
+          }}
+          component={PeopleScreen}
+        />
+        <Tab.Screen
+          name="Decision"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Image
+                source={require("../assets/decision.jpg")}
+                style={{ width: 32, height: 32, tintColor: color }}
+              />
+            ),
+          }}
+          component={DecisionScreen}
+        />
+        <Tab.Screen
+          name="Restaurants"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Image
+                source={require("../assets/restaurant.jpg")}
+                style={{ width: 32, height: 32, tintColor: color }}
+              />
+            ),
+          }}
+          component={RestaurantsScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
